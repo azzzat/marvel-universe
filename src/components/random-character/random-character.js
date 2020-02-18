0632695935e77a04da67e8d59611ebc50de2019a
azzzat <arbuz4az@yandex.ru>
2020-02-18 18:35:22 +0300

    Добавил 1 блок с рисунком

--- src/components/random-character/random-character.js
+++ src/components/random-character/random-character.js
@@ -1,26 +1,74 @@
-import React from "react";
+import React, { Component } from "react";
 
 import "./random-character.css";
 
-const RandomCharacter = () => {
-  return (
-    <div className="random-character">
-      <div className="character-image"></div>
-      <div>
-        <h3>Персонаж</h3>
-        <ul>
-          <li>
-            <span>Name</span>
-            <span>name</span>
-          </li>
-          <li>
-            <span>Year</span>
-            <span>year</span>
-          </li>
-        </ul>
+import SwapiService from "../../services/swapi-service";
+
+class RandomCharacter extends Component {
+  constructor() {
+    super();
+    this.updateCharacter();
+  }
+
+  state = {
+    name: null,
+    id: null,
+    comics: null,
+    series: null,
+    stories: null,
+    image: null
+  };
+
+  swapiService = new SwapiService();
+
+  updateCharacter() {
+    this.swapiService.getCharacter("1009610").then(character => {
+      this.setState({
+        name: character.data.results[0].name,
+        id: character.data.results[0].id,
+        comics: character.data.results[0].comics.available,
+        series: character.data.results[0].series.available,
+        stories: character.data.results[0].stories.available,
+        image:
+          character.data.results[0].thumbnail.path +
+          "." +
+          character.data.results[0].thumbnail.extension
+      });
+    });
+  }
+
+  render() {
+    const { name, id, comics, series, stories, image } = this.state;
+
+    return (
+      <div className="random-character">
+        <div className="character-image">
+          <img className="character-image" src={image} alt={id + "RandomImg"} />
+        </div>
+        <div>
+          <h2>{name}</h2>
+          <ul>
+            <li>
+              <span>ID: </span>
+              <span>{id}</span>
+            </li>
+            <li>
+              <span>Comics: </span>
+              <span>{comics}</span>
+            </li>
+            <li>
+              <span>Series: </span>
+              <span>{series}</span>
+            </li>
+            <li>
+              <span>Stories: </span>
+              <span>{stories}</span>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default RandomCharacter;
--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -1,37 +1,40 @@
-// потом
+class SwapiService {
+  getResourse = async url => {
+    const res = await fetch(
+      `https://gateway.marvel.com/v1/public/${url}?ts=1&apikey=3bc96d4bd6e4433bbdc3ad4ee302904c&hash=c439a02b89d34016a1cbace6708c4ed9`
+    );
 
-// class SwapiService {
-//   getResourse = async url => {
-//     const res = await fetch(
-//       `https://gateway.marvel.com/v1/public/${url}?ts=1&apikey=3bc96d4bd6e4433bbdc3ad4ee302904c&hash=c439a02b89d34016a1cbace6708c4ed9`
-//     );
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}`);
+    }
 
-//     if (!res.ok) {
-//       throw new Error(`Could not fetch ${url}`);
-//     }
+    return await res.json();
+  };
 
-//     return await res.json();
-//   };
+  //   getAllCharacters = async () => {
+  //     const res = await this.getResourse(`characters`);
+  //   };
 
-//   getAllCharacters = async () => {
-//     const res = await this.getResourse(`characters`);
+  getCharacter = async id => {
+    const character = await this.getResourse(`characters/${id}`);
 
-//   };
+    return character;
+  };
 
-//   getCharacter = async id => {
-//     const character = await this.getResourse(`characters/${id}`);
-//     return this.
-//   };
+  //   getcharacterImage = async id => {
+  //     const character = await this.getResourse(`characters/${id}`);
 
-//   getcharacterImage;
+  //     const img =
+  //   }
 
-//   characterData = (character) => {
-//       return {
-//           id: character.id,
-//           name: character.name,
-//           description: character.description,
-//           modified: character.modified,
-//       }
-//   }
+  characterData = character => {
+    return {
+      id: character.id,
+      name: character.name,
+      description: character.description,
+      modified: character.modified
+    };
+  };
+}
 
-// }
+export default SwapiService;
