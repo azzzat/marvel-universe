72586f06091e8c6af86141289f9bf0bc2a4f8287
azzzat <arbuz4az@yandex.ru>
2020-05-23 18:23:40 +0300

    поисковая строка

---
+++ src/components/searching-pannel/searching-pannel.js
@@ -0,0 +1,19 @@
+// import React, { Component } from "react";
+
+// import "./searching-pannel.css";
+
+// class SearchingPannel extends Component {
+//     render() {
+
+//         return(
+//             <div className="form-group">
+//                 <fieldset disabled="">
+//                 <label className="control-label" for="disabledInput">Disabled input</label>
+//                 <input className="form-control" id="disabledInput" type="text" placeholder="Character`s name" disabled="">
+//                 </fieldset>
+
+//             </div>
+
+//         )
+//     }
+// };
